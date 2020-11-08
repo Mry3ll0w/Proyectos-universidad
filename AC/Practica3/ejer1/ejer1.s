596aa7da7abaf813c36d32b4227767585c5f9214
ara <antonioroldanandrade@gmail.com>
2020-11-08 22:54:44 +0100

    Practicas de AC corregidas y listas (p1,p2)

---
+++ AC/Practica3/ejer1/ejer1.s
@@ -0,0 +1,10 @@
+    AREA ejer1,CODE,READWRITE
+EXIT EQU &11
+
+    ENTRY
+                         ;Cree un programa que reserve e inicialice 4 bytes en memoria 
+                         ;(el valor de los bytes es de tu eleccion). 
+                         ;El programa debera sumar los tres primeros bytes 
+                         ;y almacenar el resultado en el cuarto byte.
+
+    END
