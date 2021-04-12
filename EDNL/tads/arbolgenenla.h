6206b8950174579c319c6a888622b33da699d3eb
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-04-12 21:29:50 +0200

    Practicas de BD t3 parte 1

---
+++ EDNL/tads/arbolgenenla.h
@@ -0,0 +1,8 @@
+//
+// Created by anton on 22/03/2021.
+//
+
+#ifndef EDNL_ARBOLGENENLA_H
+#define EDNL_ARBOLGENENLA_H
+
+#endif //EDNL_ARBOLGENENLA_H
---
+++ EDNL/tads/practica3.h
@@ -0,0 +1,8 @@
+//
+// Created by anton on 22/03/2021.
+//
+
+#ifndef EDNL_PRACTICA3_H
+#define EDNL_PRACTICA3_H
+
+#endif //EDNL_PRACTICA3_H
