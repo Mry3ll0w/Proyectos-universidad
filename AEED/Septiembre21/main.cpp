c5259b601b18d1397a0e44e031f289d6f527e7e9
Antonio Roldan Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2021-08-13 17:43:36 +0200

    Tad Diccionario en diccionario.h

--- AEED/Septiembre21/main.cpp
+++ AEED/Septiembre21/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;//Avoid using std:: ....
-#include "tad_texto.hpp"
-#include "p2.hpp"
-//#include "examenes/feb2019.h"
-#include "tads p/pila_enla.h"
-#include "tads p/Cola_Din.h"
+#include "examenes/estacion_tren.h"
 #include "tads p/ListaEnla.h"
-#include "examenes/hospital.h"
 
 int main(){
-
+    ListaEnla<int> a;
+    a.insertar(0,a.fin());
+    a.insertar(1,a.fin());
+    a.insertar(2,a.fin());
+    a.insertar(3,a.fin());
+    a.insertar(4,a.fin());
+    auto i = a.primera();
+    while(i != a.fin()){
+        std::cout<<a.elemento(i)<<std::endl;
+        i = a.siguiente(i);
+    }
 }
