5e3bed0e11c01efb3c95f8da6a1b968c19ff0f57
Antonio Roldan Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2021-08-15 12:14:37 +0200

    Tad consultorio en consultorio.h.

--- AEED/Septiembre21/main.cpp
+++ AEED/Septiembre21/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 using namespace std;//Avoid using std:: ....
-#include "examenes/estacion_tren.h"
+#include "examenes/consultorio.h"
 #include "tads p/ListaEnla.h"
 
 int main(){
-    ListaEnla<int> a;
-    a.insertar(0,a.fin());
-    a.insertar(1,a.fin());
-    a.insertar(2,a.fin());
-    a.insertar(3,a.fin());
-    a.insertar(4,a.fin());
-    auto i = a.primera();
-    while(i != a.fin()){
-        std::cout<<a.elemento(i)<<std::endl;
-        i = a.siguiente(i);
-    }
+
+
 }
